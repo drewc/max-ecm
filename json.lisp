8518ee436e98aad54f95a96b74dcf159d3012a77
Drew Crampsie <me@drewc.ca>
2014-02-14 15:42:07 -0800

    json: add MAKE-UPDATE-JSO
     * This is needed by maxclaims for JSON RPC

--- json.lisp
+++ json.lisp
@@ -4,6 +4,22 @@
 		#:jso
 		#:write-json-to-string)
   (:export #:jso
-	   #:write-json-to-string))
+	   #:write-json-to-string
+	   #:make-update-jso))
 (in-package :max-ecm/json)
+
+(defun make-update-jso (name id field value &key (select-objects nil))
+  (max-ecm/json:write-json-to-string
+   (max-ecm/json:jso
+    "object"
+    (string-downcase name)
+    "id"
+    id
+    "field"
+    (string-downcase field)
+    "value"
+    (or value :null)
+    "select_objects"
+    (or (and select-objects (princ-to-string select-objects))
+	:false))))
 
