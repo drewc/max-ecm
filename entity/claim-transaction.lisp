64215ff8c779748a02dc0acca5565f84e105854e
Drew Crampsie <me@drewc.ca>
2014-10-17 14:06:06 -0700

    Add update for "claim-transaction approved'

--- entity/claim-transaction.lisp
+++ entity/claim-transaction.lisp
@@ -16,7 +16,9 @@
    (cheque-number :initarg :cheque-number
 	 :accessor claim-transaction-cheque-number)
    (payee-id :initarg :payee-id
-	     :accessor claim-transaction-payee-id)))
+	     :accessor claim-transaction-payee-id)
+   (approved :initarg :approved
+	     :accessor claim-transaction-approved)))
 
 (defmethod object-jso ((object claim-transaction) &key &allow-other-keys)
   (max-ecm/json:jso 
@@ -28,7 +30,8 @@
    "payee" (let ((id (claim-transaction-payee-id object)))
 	     (if (and id (not (eql id :null)))
 		 (object-jso (find-record "person" id))
-		 id))))
+		 id))
+   "approved" (claim-transaction-approved object)))
 
 (defun find-claim-transaction (id)
   (apply #'make-instance 'claim-transaction 	 
@@ -39,26 +42,37 @@
   
 (setf (find-record "claim-transaction")
       'find-claim-transaction)
+
+(defun from-bool (thing)
+  (cond ((string= thing "%:false:%")
+	 nil)
+	((string= thing "on")
+	 t)	
+	(t (from-json-bool thing))))
 		
 (defmethod update-record/jso  ((record claim-transaction) jso)
   (let ((cheque-number (getjso "cheque_number" jso)))
     (multiple-value-bind (payee payee-exists?)
 	(getjso "payee" jso)
+      (multiple-value-bind (approved approved-exists?)
+	  (getjso "approved" jso)
     (if (and (not cheque-number)
-	     (not payee-exists?))
+	     (not payee-exists?)
+	     (not approved-exists?))
 	(error "Nothing to update afor ~A" jso)
 	(postmodern:execute 
 	 (s-sql:sql-compile 
 	  `(:update claim-transaction 
 	    :set 
 	    ,@(when cheque-number (list 'cheque-number cheque-number))
+	    ,@(when approved-exists? (list 'approved (from-bool approved)))
 	    ,@(when payee-exists? (list 'payee-id (etypecase payee
 						   (integer payee)
 						   (string (read-from-string payee))
 						   (symbol payee))))
 	    :where (:= transaction-id
 		       ,(record-id record))))))
-    (find-record "claim-transaction" (record-id record)))))
+    (find-record "claim-transaction" (record-id record))))))
 
 
 
